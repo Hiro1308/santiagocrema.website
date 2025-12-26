96ef984aae0c2136d947456d7d2c30e6326a4e42
Pablo Gomez <78317032+Hiro1308@users.noreply.github.com>
2025-12-26 04:16:10 -0300

    xd

---
+++ audio/music/playlist.js
@@ -0,0 +1,18 @@
+const playlist = [
+  { title: "Can You Hear The Music", sub: "Oppenheimer", file: "audio/music/can_you_hear_the_music_oppenheimer.mp3" },
+  { title: "Highscore", sub: "Teminite & Panda Eyes", file: "audio/music/highscore_teminite_panda_eyes.mp3" },
+  { title: "Immigrant Song", sub: "Led Zeppelin", file: "audio/music/immigrant_song_led_zeppelin.mp3" },
+  { title: "Impatience", sub: "Jujutsu Kaisen", file: "audio/music/impatience_jujutsu_kaisen.mp3" },
+  { title: "Intro", sub: "ALF", file: "audio/music/intro_alf.mp3" },
+  { title: "Jimmy White's Cueball", sub: "Manfred Linzner", file: "audio/music/jimmy_whites_cueball_manfred_linzner.mp3" },
+  { title: "Lux Aeterna", sub: "Requiem for a Dream", file: "audio/music/lux_aeterna_requiem_for_a_dream.mp3" },
+  { title: "Main Theme", sub: "Grand Theft Auto 2", file: "audio/music/main_theme_grand_theft_auto_2.mp3" },
+  { title: "My Heart Will Go On (Instrumental)", sub: "Celline Dion", file: "audio/music/my_heart_will_go_on_instrumental_celline_dion.mp3" },
+  { title: "Nine Thou (Grant Mohrman Supertars Remix)", sub: "Styles Of Beyond", file: "audio/music/nine_thou_grant_mohrman_supertars_remix_styles_of_beyond.mp3" },
+  { title: "OST 1-08 BOX 15", sub: "DANGANRONPA", file: "audio/music/ost_1_08_box_15_danganronpa.mp3" },
+  { title: "Revengers", sub: "Plus Ultra", file: "audio/music/revengers_plus_ultra.mp3" },
+  { title: "The Only Thing They Fear Is You (Mick Gordon)", sub: "Doom Eternal OST", file: "audio/music/the_only_thing_they_fear_is_you_mick_gordon_doom_eternal_ost.mp3" },
+  { title: "Track 8 (CD Quality)", sub: "Sheep Raider OST", file: "audio/music/track_8_cd_quality_sheep_raider_ost.mp3" },
+  { title: "Trinity", sub: "009 Sound System", file: "audio/music/trinity_009_sound_system.mp3" },
+  { title: "What I've Done (Instrumental)", sub: "Linkin Park", file: "audio/music/what_ive_done_instrumental_linkin_park.mp3" }
+];
